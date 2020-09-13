b33c24afc8b9af5604451932a0f7af7f5e1c34e1
Michael Mohamed <46542366+michaelmohamed@users.noreply.github.com>
2020-09-12 23:57:24 -0400

    Update init.sh

--- ubuntu18/init.sh
+++ ubuntu18/init.sh
@@ -36,8 +36,8 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 echo "Installing docker-compose..."
-curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 echo "Installing nvidia docker..."
